f3caddbcf8c216e53bbaaa3ae14a8297a86aea3f
Sergey Yakovenko <development@satylab.com>
2019-04-25 22:10:39 +0300

    icons added to drawable

--- app/src/main/java/app/wallpaper/widget/BottomNavigationView.kt
+++ app/src/main/java/app/wallpaper/widget/BottomNavigationView.kt
@@ -2,15 +2,9 @@ package app.wallpaper.widget
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
-import androidx.constraintlayout.widget.ConstraintLayout
-import app.wallpaper.R
-import butterknife.ButterKnife
-import butterknife.Unbinder
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class BottomNavigationView : ConstraintLayout {
-
-    private var unbinder: Unbinder
+class NavigationView : BottomNavigationView {
 
     @JvmOverloads
     constructor(
@@ -18,15 +12,4 @@ class BottomNavigationView : ConstraintLayout {
             attrs: AttributeSet? = null,
             defStyleAttr: Int = 0
     ) : super(context, attrs, defStyleAttr)
-
-    init {
-        val view = View.inflate(context, R.layout.view_bottom_navigation, this)
-        unbinder = ButterKnife.bind(this, view)
-    }
-
-    override fun onDetachedFromWindow() {
-        super.onDetachedFromWindow()
-
-        unbinder?.unbind()
-    }
 }
